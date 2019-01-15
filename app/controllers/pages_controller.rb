943c7f0ad40ab6ebaa01590306f0d311c4ed05e0
matthieu delegrange <mdelegrange@gmail.com>
2019-01-15 10:20:24 +0100

    Edited pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,25 @@
 class PagesController < ApplicationController
   def home
+    @is_home = params[:controller] == "pages" && params[:action] == "home"
+  end
+
+  def contact
+  end
+
+  def liens
+    @posts = Post.all
+    @liens = Lien.all
+  end
+
+  def cliniciens
+  end
+
+  def psy
+  end
+
+  def patient
+  end
+
+  def pratiques
   end
 end
