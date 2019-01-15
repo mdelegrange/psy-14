5e9065444f31111812c80e72b356a1199fe39d9c
matthieu delegrange <mdelegrange@gmail.com>
2019-01-15 10:22:08 +0100

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'contact', to: 'pages#contact'
+  get 'liens', to: 'pages#liens'
+  get 'cliniciens', to: 'pages#cliniciens'
+  get 'vous-cherchez-un-psy', to: 'pages#psy'
+  get 'nous-orienter-un-patient', to: 'pages#patient'
+  get 'pratiques-professionnelles', to: 'pages#pratiques'
+
+  resources :posts, only: [:index, :create, :destroy]
+  resources :liens, only: [:create, :destroy]
 end
